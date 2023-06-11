c332107bf0be36bd57cc7b1d70ec69c874f24492
Anatoliy Matsakov <mactolik1993@gmail.com>
2023-06-11 17:16:15 +0300

    add fonts and first folders

--- App.js
+++ App.js
@@ -1,20 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, SafeAreaView, FlatList } from "react-native";
+import { useFonts } from "expo-font";
+
 
 export default function App() {
+
+  const [fontsLoaded] = useFonts({
+    "Roboto-Bold": require("./src/assets/fonts/Roboto-Bold.ttf"),
+    "Roboto-Medium": require("./src/assets/fonts/Roboto-Medium.ttf"),
+    "Roboto-Regular": require("./src/assets/fonts/Roboto-Regular.ttf"),
+  });
+
+
+
+ if (!fontsLoaded) {
+   return null;
+ }
+
   return (
-    <View style={styles.container}>
-      <Text>Hello world!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text style={{ fontFamily: "Roboto-Medium", fontSize: 30 }}>
+        Hello All
+      </Text>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#ecf0f1",
   },
 });
